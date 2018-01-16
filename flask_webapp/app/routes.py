b160ae930a87e7300caba44f9f346fa504c4e425
y.wilke <y.wilke@protonmail.com>
2018-01-16 22:56:17 +0100

    Line chart replaced with XY chart. X en Y value formatting added.

--- flask_webapp/app/routes.py
+++ flask_webapp/app/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 import pygal
+import datetime
 
 with open("D:/Yano/Drive/Other/python/auctionator_history/auction_history.txt","r") as auction_history_file: # Load dict of auction history
     auction_history = {}
@@ -9,6 +10,21 @@ with open("D:/Yano/Drive/Other/python/auctionator_history/auction_history.txt","
         auction_history = eval(auction_history_file.read())
     except SyntaxError:
         pass
+
+def copper_to_price(copper): # Converts copper int to a normal price
+    copper = int(copper)
+    s, c = divmod(copper, 100)
+    g, s = divmod(s, 100)
+    if g == 0:
+        if s == 0:
+            return '{}c'.format(c)
+        else:
+            return '{}s{}c'.format(s, c)
+    else:
+        return '{}g{}s{}c'.format(g, s, c)
+
+def get_date(unix_time):# Converts unix time dd-mm-yy
+    return datetime.datetime.fromtimestamp(unix_time).strftime('%d-%m-%y')
 
 @app.route('/')
 @app.route('/index')
@@ -22,14 +38,12 @@ def login():
         for item in auction_history:
             if form.item_search.data.lower() == item.lower():
                 price_list = []
-                date_list = []
-                for data_point in auction_history[item]:
-                    price_list.append(int(data_point[1]))
-                    date_list.append(int(data_point[0]))
-                chart = pygal.Line()
+                for datapoint in auction_history[item]:
+                    price_list.append((int(datapoint[0]),int(datapoint[1])))
+                chart = pygal.XY(x_label_rotation=35, value_formatter=lambda y: copper_to_price(y), x_value_formatter=lambda x: get_date(x))
                 chart.title = "{}'s price history".format(item)
-                chart.x_labels = date_list
                 chart.add(item, price_list)
                 chart = chart.render_data_uri()
                 return render_template('Lordaeron_AH_history.html', title='Search AH history', form=form, chart=chart)
     return render_template('Lordaeron_AH_history.html', title='Search AH history', form=form)
+
