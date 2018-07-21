827942221c09de074b31341053873aa3f960fd09
unknown <y.wilke@protonmail.com>
2018-07-22 01:53:04 +0200

    some PEP8 changes

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,21 @@
+import datetime
+import sqlite3 as sqlite
+from statistics import median
+
 from flask import render_template, request
-from app import app
 import plotly.offline as plotly
 import plotly.graph_objs as plotgo
 import pandas as pd
-import datetime
-import sqlite3 as sqlite
 import numpy as np
+
+from app import app
 
 con = sqlite.connect('import/auctionhistory.db') #db path
 cur = con.cursor()
 
-def copper_to_price(copper): # Converts copper int to a normal price
+
+def copper_to_price(copper):
+    '''Return the price in WoW currency.'''
     copper = int(copper)
     s, c = divmod(copper, 100)
     g, s = divmod(s, 100)
@@ -23,7 +28,8 @@ def copper_to_price(copper): # Converts copper int to a normal price
         return '{}g{}s{}c'.format(g, s, c)
 
 
-def get_date(unix_time):# Converts unix time dd-mm-yy
+def get_date(unix_time):
+    '''Return date in dd-mm-yyyy.'''
     return datetime.datetime.fromtimestamp(unix_time).strftime('%d-%m-%y')
 
 @app.route('/')
@@ -38,147 +44,147 @@ def search(server_arg):
     AH_title = '{} Auction House History'.format(server_arg.replace('_', ' '))
     html_page = 'search.html'
     epoch_now = int(datetime.datetime.now().timestamp())
-    time_dir = {'30d' : 2592000, '3m' : 7890000, '1y' : 31536000, 'all' : epoch_now}
+    time_dir = {'30d': 2592000,
+                '3m': 7890000,
+                '1y': 31536000,
+                'all': epoch_now}
     try: 
         scantime = epoch_now - time_dir[time_arg]
     except KeyError:
         scantime = None
     
-    if search_arg and scantime:
-        query = (search_arg, server_arg, scantime)
-        cur.execute("SELECT itemid FROM items "
-                    "WHERE itemname IS ? ;", (search_arg,))
-        
-        if cur.fetchone():
-            cur.execute("SELECT itemname, price, scantime FROM prices "
-                        "INNER JOIN items ON prices.itemid=items.itemid "
-                        "INNER JOIN scans ON prices.scanid=scans.scanid "
-                        "INNER JOIN servers ON prices.serverid=servers.serverid "
-                        "WHERE items.itemname IS ? "
-                        "AND servers.servername IS ? "
-                        "AND scans.scantime > ?;", query)
-            datapoints = sorted(cur.fetchall(), key=lambda x: x[2])
-            if not datapoints:
-                return render_template(html_page,
-                                       title='No prices available',
-                                       AH_title=AH_title,
-                                       error='This item has not been listed in the selected time range.',
-                                       value=search_arg,
-                                       tvalue=time_arg)
-        else:
-            if len(search_arg) < 3:
-                return render_template(html_page,
-                                       title='Item not found',
-                                       AH_title=AH_title,
-                                       error='Type at least 3 characters.',
-                                       value=search_arg,
-                                       tvalue=time_arg)
-            
-            cur.execute("SELECT itemname FROM items WHERE itemname LIKE ?;",
-                       ('{0}{search}{0}'.format('%', search=query[0]),))
-            item_matches = sorted(cur.fetchall(), key=lambda x: len(x[0]))
-            
-            if item_matches:
-                item_suggestions = []
-                for match in item_matches:
-                    href_display = match[0]
-                    href_item = match[0].replace(' ', '+')
-                    href = '/server/{server_arg}?search={item}&time={time}'.format(
-                            server_arg=server_arg, item=href_item, time=time_arg )
-                    item_suggestions.append((href_display, href))
-                return render_template(html_page,
-                                       title=AH_title,
-                                       AH_title=AH_title,
-                                       suggestions=item_suggestions,
-                                       value=search_arg,
-                                       tvalue=time_arg)
-            
-            else:
-                return render_template(html_page,
-                                       title='Item not found',
-                                       AH_title=AH_title,
-                                       error='Item was not found in the database.',
-                                       value=search_arg,
-                                       tvalue=time_arg)
-        
-        # Format data from query
-        item = datapoints[0][0]
-        time_list = []
-        price_list = []
-        for i in datapoints:
-            time_list.append(i[2])
-            price_list.append(i[1])
-        # Generate moving average
-        window = '5D'
-        index = pd.to_datetime(time_list, unit = 's')
-        df = pd.DataFrame({'prices': price_list}, index)
-        dfr = df.rolling('5D').mean()
-        # Create traces
-        trace_price = plotgo.Scattergl(
-        x = index,
-        y = price_list,
-        text = list(map(copper_to_price, price_list)),
-        hoverinfo = 'text+x',
-        name = item,
-        mode = 'markers',
-        )
-        
-        trace_avg = plotgo.Scattergl(
-        x = dfr.axes[0],
-        y = dfr['prices'],
-        text = list(map(copper_to_price, dfr['prices'])),
-        hoverinfo = 'text+x',
-        name = 'average ({window})'.format(window = window),
-        mode = 'lines',
-        )
-        
-        plotdata = [trace_price, trace_avg]
-
-        # Layout
-        max_val = max(price_list)
-        nr_ticks = 6
-        y_val = 0
-        y_vals = []
-        
-        t1 = (max_val / nr_ticks)
-        t2 = int(np.log10(t1))
-        t3 = int(t1 / (10**t2)) + 1
-        step = t3*10**t2
-        for i in range(nr_ticks):
-            y_val += step
-            y_vals.append(y_val)
-        
-        layout = plotgo.Layout(
-            title = "{item}'s price history".format(item = item),
-            font = dict(
-                color = '#ffffff'    
-            ),
-            yaxis=dict(
-                gridcolor='rgba(26, 26, 26, 0.6)',
-                tickvals = y_vals,
-                ticktext = list(map(copper_to_price, y_vals))
-            ),
-            xaxis = dict(
-                gridcolor='rgba(26, 26, 26, 0.2)',
-                hoverformat = '%e %b %Y'
-            ),
-            paper_bgcolor='#263238',
-            plot_bgcolor='#263238'
-        )
-
-        fig = dict(data=plotdata, layout=layout)
-        chart = plotly.offline.plot(fig, include_plotlyjs=False, output_type="div")
-        return render_template(html_page,
-                               title=AH_title,
-                               AH_title=AH_title,
-                               chart=chart,
-                               value=search_arg,
-                               tvalue=time_arg)
-    
-    else:
-        return render_template(html_page,
-                               title=AH_title,
-                               AH_title=AH_title,
+    if not search_arg or not scantime:
+        return render_template(html_page, title=AH_title, AH_title=AH_title,
                                tvalue='3m')
+    query = (search_arg, server_arg, scantime)
+    cur.execute("SELECT itemid FROM items "
+                "WHERE itemname IS ? ;", (search_arg,))
+    
+    if not cur.fetchone():  # no direct match
+        if len(search_arg) < 3:
+            return render_template(html_page, title='Item not found',
+                                   AH_title=AH_title, 
+                                   error='Type at least 3 characters.',
+                                   value=search_arg, tvalue=time_arg)
+        
+        cur.execute("SELECT itemname FROM items WHERE itemname LIKE ?;",
+                   ('{0}{search}{0}'.format('%', search=query[0]),))
+        item_matches = sorted(cur.fetchall(), key=lambda x: len(x[0]))
+        if item_matches:
+            item_suggestions = []
+            for match in item_matches:
+                href_display = match[0]
+                href_item = match[0].replace(' ', '+')
+                href = '/server/{server_arg}?search={item}&time={time}'.format(
+                        server_arg=server_arg, item=href_item, time=time_arg)
+                item_suggestions.append((href_display, href))
+            return render_template(html_page, title=AH_title,
+                                   AH_title=AH_title,
+                                   suggestions=item_suggestions,
+                                   value=search_arg, tvalue=time_arg)
+        else:
+            return render_template(html_page, title='Item not found',
+                                   AH_title=AH_title,
+                                   error='Item was not found in the database.',
+                                   value=search_arg, tvalue=time_arg)
+        
+    cur.execute("SELECT itemname, price, scantime FROM prices "
+                "INNER JOIN items ON prices.itemid=items.itemid "
+                "INNER JOIN scans ON prices.scanid=scans.scanid "
+                "INNER JOIN servers ON prices.serverid=servers.serverid "
+                "WHERE items.itemname IS ? "
+                "AND servers.servername IS ? "
+                "AND scans.scantime > ?;", query)
+    datapoints = sorted(cur.fetchall(), key=lambda x: x[2])
+    if not datapoints:
+        not_found = "This item has not been listed in the selected time range."
+        return render_template(html_page, title='No prices available',
+                               AH_title=AH_title, error=not_found,
+                               value=search_arg, tvalue=time_arg)
+    '''
+    # Calculate MAD
+    prices = []
+    for i in datapoints:
+        prices.append(i[1])
+    median_price = (median(prices))
+    diffs_median = []
+    for price in prices:
+        diffs_median.append(abs(price-median_price))
+    mad = (median(diffs_median))
+    print (mad)
+    print (median_price)
+    diff_mult = []
+    for i in diffs_median:
+        diff_mult.append(int(i/mad))
+    print (diff_mult)
+    '''
+    
+    # Format data from query
+    item = datapoints[0][0]
+    time_list = []
+    price_list = []
+    for i in datapoints:
+        time_list.append(i[2])
+        price_list.append(i[1])
+    # Generate moving average
+    window = '5D'
+    index = pd.to_datetime(time_list, unit = 's')
+    df = pd.DataFrame({'prices': price_list}, index)
+    dfr = df.rolling(window).mean()
+    # Create traces
+    trace_price = plotgo.Scattergl(
+    x = index,
+    y = price_list,
+    text = list(map(copper_to_price, price_list)),
+    hoverinfo = 'text+x',
+    name = item,
+    mode = 'markers',
+    )
+    
+    trace_avg = plotgo.Scattergl(
+    x = dfr.axes[0],
+    y = dfr['prices'],
+    text = list(map(copper_to_price, dfr['prices'])),
+    hoverinfo = 'text+x',
+    name = 'average ({window})'.format(window = window),
+    mode = 'lines',
+    )
+    plotdata = [trace_price, trace_avg]
+    # Layout
+    max_val = max(price_list)
+    nr_ticks = 6
+    y_val = 0
+    y_vals = []
+    
+    _t1 = (max_val / nr_ticks)
+    _t2 = int(np.log10(_t1))
+    _t3 = int(_t1 / (10**_t2)) + 1
+    step = _t3 * 10**_t2
+    for i in range(nr_ticks):
+        y_val += step
+        y_vals.append(y_val)
+    
+    layout = plotgo.Layout(
+        title = "{item}'s price history".format(item=item),
+        font = dict(
+            color = '#ffffff'    
+        ),
+        yaxis=dict(
+            gridcolor='rgba(26, 26, 26, 0.6)',
+            tickvals = y_vals,
+            ticktext = list(map(copper_to_price, y_vals))
+        ),
+        xaxis = dict(
+            gridcolor='rgba(26, 26, 26, 0.2)',
+            hoverformat = '%e %b %Y'
+        ),
+        paper_bgcolor='#263238',
+        plot_bgcolor='#263238'
+    )
+
+    fig = dict(data=plotdata, layout=layout)
+    chart = plotly.offline.plot(fig, include_plotlyjs=False, output_type="div")
+    return render_template(html_page, title=AH_title, AH_title=AH_title,
+                           chart=chart, value=search_arg, tvalue=time_arg)
 
 
